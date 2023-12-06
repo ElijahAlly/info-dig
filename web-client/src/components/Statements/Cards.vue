9856d204219f5704d8abbf0ff4270a3df4ca75a3
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-12-06 01:40:28 -0500

    added frontend pages

---
+++ web-client/src/components/Statements/Cards.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    props: {
+        msg: String,
+    },
+})
+export default class Cards extends Vue {
+    msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only
+* // Todo: Implement scroll snapping -> https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_scroll_snap/Basic_concepts -->
+<style scoped lang="scss">
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}</style>
---
+++ web-client/src/views/StatementsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="statements">
+        <h1>This is the statements page :)</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Cards from "@/components/Statements/Cards.vue";
+
+@Options({
+    components: {
+        Cards,
+    },
+})
+
+export default class StatementsView extends Vue { }
+</script>
